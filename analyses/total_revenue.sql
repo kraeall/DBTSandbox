200408b67a0e3bf36bb570385076974152dd5a3e
kraeall <kraeallison@gmail.com>
2024-05-04 17:28:51 -0700

    changed total rev model

--- analyses/total_revenue.sql
+++ analyses/total_revenue.sql
@@ -1,20 +1,12 @@
-with payments as(
-
-select * from {{ ref('stg_payments')}}
-
-),
-
-staged as  {
-    select
-
-    payment_id,
-    order_id,
-    payment_method,
-    amount sum(amount) 
-
-    from payments
-    group by 1
-
-}
-
-select * from staged
+with payments as
+    ( select * from {{ ref('stg_payments') }} 
+ 
+ ), 
+ 
+        aggregated as ( 
+            select sum(amount) as total_revenue 
+            
+            from payments 
+            where status = 'success' ) 
+            
+select * from aggregated
