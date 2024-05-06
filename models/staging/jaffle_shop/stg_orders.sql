5f2b26f02e095905e12526c1ae177d880bd90ce9
kraeall <kraeallison@gmail.com>
2024-05-06 13:17:18 -0700

    semantics layer course additions

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -7,10 +7,25 @@ select * from {{ source('jaffle_shop', 'raw_orders') }}
 renamed as (
 
     select
+         ----------  ids
         id as order_id,
         user_id as customer_id,
-        order_date,
-        status
+        status,
+
+        ---------- store_id as location_id,
+        ---------customer as customer_id,
+
+        ---------- numerics
+        --------subtotal as subtotal_cents,
+        --------tax_paid as tax_paid_cents,
+        --------order_total as order_total_cents,
+        -------------user_id as customer_id,
+        ----------{{ cents_to_dollars('subtotal') }} as subtotal,
+        ---------{{ cents_to_dollars('tax_paid') }} as tax_paid,
+        -----------{{ cents_to_dollars('order_total') }} as order_total,
+
+        ---------- timestamps
+        {{ dbt.date_trunc('day','order_date') }} as order_date
 
     from source
 
